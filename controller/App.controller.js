e797eb3621cfa4c488c0188df5b088124950aee6
michel <micheltallon@gmail.com>
2016-07-16 22:24:33 +0200

    Ajout du Json model

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,12 +1,23 @@
 sap.ui.define([
-   "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], function (Controller, MessageToast) {
-   "use strict";
-   return Controller.extend("ui5.template.controller.App", {
-      onShowHello : function () {
-         // show a popup with Toast
-          MessageToast.show("Hello World");
-      }
-   });
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
+    "use strict";
+    return Controller.extend("ui5.template.controller.App", {
+        onInit : function () {
+            // set data model on view
+            var oData = {
+                recipient : {
+                    name : "World"
+                }
+            };
+            var oModel = new JSONModel(oData);
+            this.getView().setModel(oModel);
+        },
+        onShowHello : function () {
+            // show a popup with Toast
+            MessageToast.show("Hello World");
+        }
+    });
 });

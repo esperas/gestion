15aea8679f1004db48cc861d256aa051187a4600
michel <micheltallon@gmail.com>
2016-07-17 22:26:07 +0200

    Ajout d'un panneau latéral avec un menu

---
+++ controller/Menu.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], function (Controller, MessageToast) {
+    "use strict";
+    return Controller.extend("ui5.template.controller.Menu", {
+
+
+    });
+});

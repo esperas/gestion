989344778adb6d6eecd96a50d8b08198764c1828
michel <micheltallon@gmail.com>
2016-07-20 21:58:23 +0200

    version ultra basique mono view

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,10 +1,23 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], function (Controller, MessageToast) {
+    'jquery.sap.global',
+    'sap/ui/core/mvc/Controller',
+    'sap/ui/model/json/JSONModel'
+], function(jQuery, Controller, JSONModel) {
     "use strict";
-    return Controller.extend("ecole.famille.controller.App", {
+
+    var TableController = Controller.extend("ecole.famille.controller.App", {
+
+        onInit: function () {
+            // set explored app's demo model on this sample
+            var oModel = new sap.ui.model.json.JSONModel("./json/data.json");  //use path relative to the Root
+
+            sap.ui.getCore().setModel(oModel);
 
 
+        }
     });
+
+
+    return TableController;
+
 });

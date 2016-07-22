0f9418def421975ffb4f4ca854ae739f368b8f1d
michel <micheltallon@gmail.com>
2016-07-23 00:18:39 +0200

    Mise à jour du modèle "coucou" à partir d'un fichier JSON locale

--- Component.js
+++ Component.js
@@ -12,13 +12,13 @@ sap.ui.define([
             // call the init function of the parent
             UIComponent.prototype.init.apply(this, arguments);
             // set data model
-            var oModel = new sap.ui.model.json.JSONModel("json/data.json")
+            /*var oModel = new sap.ui.model.json.JSONModel("json/data.json")
             .attachRequestCompleted( function() {
                 console.log("Load Json local", oModel);
                 sap.ui.getCore().setModel(oModel,"famille");
                 oModel.refresh();
                 return oModel;
-            } );
+            } );*/
             console.log("Ligne de code suivante");
             //var oModel = new JSONModel("json/data.json");
 
--- controller/Suivi.controller.js
+++ controller/Suivi.controller.js
@@ -1,22 +1,50 @@
 sap.ui.define([
-   "sap/ui/core/mvc/Controller",
-   "sap/m/MessageToast"
-], function (Controller, MessageToast) {
-   "use strict";
-   return Controller.extend("ecole.famille.controller.Suivi", {
-    init : function(){
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
+    "use strict";
+    return Controller.extend("ecole.famille.controller.Suivi", {
+        onInit : function () {
+            // set data model on view
+            /*    var oModel = new sap.ui.model.json.JSONModel("json/data2.json")
+            .attachRequestCompleted( function() {
+                console.log("Load Json local 2 - Suivi ", oModel);
+                sap.ui.getCore().getView().setModel(oModel, "famille");
+                return oModel;
+            } );*/
 
-    },
-      onShowHello : function () {
-         // read msg from i18n model
-         var oBundle = this.getView().getModel("i18n").getResourceBundle();
+        },
+        onShowHello : function () {
+            // read msg from i18n model
+            var oBundle = this.getView().getModel("i18n").getResourceBundle();
+
+            var oModel = this.getView().getModel("coucou");
+            //oModel = new sap.ui.model.json.JSONModel("json/data2.json")
+            oModel.loadData("json/data2.json");
+            //.attachRequestCompleted( function() {
+            /*    var oData = oModel.getData();
+                console.log("Load Json local 2 - Suivi ", oData);
+                // Lecture du modèle interne
+                var sapModel = sap.ui.getCore().getModel("coucou");
+                console.log("Model interne : ",sapModel);
+
+                sapModel.setData(oData);
+                sapModel.refresh();
+                console.log("Famille : ",sapModel);*/
+              //  oModel.refresh();
+              //  return oModel;
+            //} );
+
+            // show message
+            var oMode/*l = this.getView().getModel("coucou");
+            var oData = oModel.getData();
+            oData.famille = "ONHELLO";
+            oModel.setData(oData);
+            oModel.refresh();
+            console.log("Famille : ",oModel);*/
 
 
-         // show message
-          var oData = sap.ui.getCore().getModel("famille");
-        console.log("Init suivi",oData);
-          oData.refresh();
-
-      }
-   });
+        }
+    });
 });

791bc8dadac79461e658917c613b09502b6c4bb5
TALLON <michel.tallon@atos.net>
2016-09-16 15:54:14 +0200

    Ajout d'un bouton pour télécharger le PDF de la facture

--- controller/Suivi.controller.js
+++ controller/Suivi.controller.js
@@ -1,10 +1,18 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
+    "../model/formatter",
     "sap/ui/model/json/JSONModel"
-], function (Controller, MessageToast, JSONModel) {
+], function (Controller, MessageToast, formatter, JSONModel) {
     "use strict";
     return Controller.extend("ecole.famille.controller.Suivi", {
+        formatter : formatter,
 
+        onLink : function(evt) {
+            var obj = evt.getSource().getBindingContext("famille").getObject();
+            console.log(obj);
+            var url = "./json/facture/" + jQuery.sap.getUriParameters().get("parent") + "-" + obj.piece + ".pdf";
+            window.open(url);
+        }
     });
 });
---
+++ model/formatter.js
@@ -0,0 +1,12 @@
+sap.ui.define([], function () {
+	"use strict";
+
+	return {
+        formatButton: function (sPiece) {
+            console.log(sPiece);
+            if (sPiece) { return true};
+            return false;
+
+        }
+	};
+});

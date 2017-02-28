52b583ce0b0fc032d5d7926a6e67400d914def8a
esperas <micheltallon@gmail.com>
2017-02-28 13:32:22 +0100

    Ajout de l'import de fichiers via API

---
+++ README.md
@@ -0,0 +1,19 @@
+#Version
+0.1.0
+Application des gestions des données de suivi des factures et des paiements
+
+
+# Description
+Application de gestion des données de suivi.
+Ce programme permet de mettre à jour les données de suivi des paiements visible par les membres de l'association.
+Il permet de mettre également à disposition les fichiers PDF de type factures et justificatifs divers.
+
+# Architecture
+L'application de gestion est la partie backend du suivi des factures et des paiements.
+Ce programme utilise une API pour sauvegarder les données de suivi.
+Les fichiers PDF de l'application sont envoyés vers une APÏ.
+
+# Technologie
+Cette appli est codé en Javascript avec le framework openUI5.
+
+
--- controller/ImportFile.controller.js
+++ controller/ImportFile.controller.js
@@ -27,38 +27,66 @@ sap.ui.define([
 			oBinding.filter(aFilter);
 
         },
+        _upload : function (fichier, tri, typedoc) {
+
+            return new Promise(function(resolve, reject) {
+                var fd = new FormData();
+                fd.append('0', fichier);
+                fd.append('tri', tri );
+                fd.append('typedoc', typedoc );
+                var api = jQuery.ajax({
+                    url : 'http://api:8080/fichiers?XDEBUG_SESSION_START=xdebug', // Specify the path to your API service
+                    type : 'POST',              // Assuming creation of an entity
+                    contentType : false,        // To force multipart/form-data
+                    mimeType: "multipart/form-data",
+                    data : fd,
+                    processData : false,
+                    success : function(data) {
+                        resolve(data);// Handle the response on success
+                            // alert(JSON.stringify(data));
+                    }
+                });
+            });
+        },
 
         onStartUpload : function(oEvent) {
 			var oUploadCollection = this.getView().byId("files");
-
+			var oTri = this.getView().byId("tri");
+            var oTypeDoc = this.getView().byId("typedoc");
 			var cFiles = oUploadCollection.getItems();
-            var newParam = new sap.ui.unified.FileUploaderParameter();
-            newParam.setName('tri');
-            newParam.setValue(this.getView().byId("tri").getValue());
 
             var i;
             var cFile;
             var toto;
+            var tmp_upload = [];  // Stockage temporaire du fileUpload
+            var promises = [];
 
             for (i = 0; i < cFiles.length; i++) {
+                cFile = cFiles[i].getFileUploader();
 
-                if (!cFiles[i].getUrl()) {
-                    cFile = cFiles[i].getFileUploader();
+                if (!cFiles[i].getUrl() && (tmp_upload.indexOf(cFile)===-1) ) {
+                    tmp_upload.push(cFile);
                     toto = sap.ui.getCore().byId(cFile);
-                    toto.setSendXHR(true);
-                    toto.removeAllHeaderParameters();
-                    toto.removeAllParameters();
-                    toto.addParameter(newParam);
-                    toto.setUseMultipart(true);
-                    toto.upload();
+                    // Pour chaque objet FileUploader, on transfert les fichers 1 par un (a cause de la limitation PHP OVH)
+                    var x;
+                    var fichier;
+                    for (x in toto.FUEl.files) {
+                        fichier = toto.FUEl.files[x];
+                        if (fichier instanceof File) {
+                            promises.push(this._upload(fichier, oTri.getValue(), oTypeDoc.getValue()));
+                        };
+                    }
                 }
-            }
-		},
+                if (!cFiles[i].getUrl()){
+                    oUploadCollection.removeItem(cFiles[i]);
+                }
 
-        onUploadComplete : function(oEvent) {
-            delete window.cachedScriptPromises.files;
-            this.getOwnerComponent().file.cachedModel( "files", "http://api:8080/fichiers", this.callok);
-        },
+            };
+            Promise.all(promises).then(function() {
+                delete window.cachedScriptPromises.files;
+                window.controller.getOwnerComponent().file.cachedModel( "files", "http://api:8080/fichiers", function(){console.log("Relecture des fichiers effectués")});
+            });
+		},
 
         done : function() {
             console.log("Demande CachedModel terminé")
